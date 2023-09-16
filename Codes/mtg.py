2f3bbd7ea58affcfba7bcf6e273500271618fab2
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-16 06:59:54 +0000

    Uploaded some files and removed old files

---
+++ Codes/mtg.py
@@ -0,0 +1,8 @@
+def finish_sentence(sentence, n, corpus, randomize=False):
+    # Sentence is the test set
+    # n is previous n tokens
+    # Corpus is your train set
+    # If randomize is false, choose the most probable next token
+
+    # Step 1 - take corpus (which is already tokenized)
+    pass
